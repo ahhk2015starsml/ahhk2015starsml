66ca005f005a73ed9e8f1bbf9af4bfa69b537e4f
ireland <none>
2015-06-21 03:33:54 +0800

    Added personality data store functionality.

--- controllers/PersonalityService.js
+++ controllers/PersonalityService.js
@@ -1,14 +1,52 @@
 'use strict';
 
-exports.postPersonality = function(body) {
+var cps = require('cps-api');
 
-  var examples = {};
-  
-  examples['application/json'] = "aeiou";
-  
+// Creating a CPS connection
+var cpsConn = new cps.Connection(
+    'tcp://cloud-us-0.clusterpoint.com:9007',
+    'ahhk2015starsml',
+    //process.env.CPS_USERNAME,
+    //process.env.CPS_PASSWORD,
+    "ahhkuser",
+    "ahhkpass",
+    'document',
+    'document/id',
+    {account: 100586});
 
-  
-  if(Object.keys(examples).length > 0)
-    return examples[Object.keys(examples)[0]];
-  
+exports.postPersonality = function (body, callback) {
+    // In Cloud version autoincrement is not yet implemented - a unique Document ID
+    // should be generated 'manually'. Using timestamp, for example.
+
+    // should be an upsert if using mongodb, instead this db is incomplete
+    var myId = body.birthday + ":"
+        + body.weightings.mind + ""
+        + body.weightings.energy + ""
+        + body.weightings.nature  + ""
+        + body.weightings.tactics + ""
+        + body.weightings.identity + ":"
+        + new Date().getTime();
+
+    var insertDoc = {
+        "id": myId,
+        "birthday": body.birthday,  // should be format '2015-06-20'
+        "weightings": [
+            body.weightings.mind,
+            body.weightings.energy,
+            body.weightings.nature,
+            body.weightings.tactics,
+            body.weightings.identity],
+        "locationLongitude": body.locationLongitude,
+        "locationLatitude": body.locationLatitude,
+        "social": body.social
+    };
+    var insertRequest = new cps.InsertRequest(insertDoc);
+    cpsConn.sendRequest(insertRequest, function (err, insertResponse) {
+        if (err) {
+            console.error(err);
+            callback(null);
+        }
+        console.log("Posting Personality: " + JSON.stringify(insertResponse));
+        callback(myId);
+    });
 }
